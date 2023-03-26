96980759e3f12a1ddafceef6e9c46554290de909
MappyJ <1603931150@qq.com>
2023-03-26 15:44:58 +0800

    change

--- AsoulFollower/obj/Release/net6.0-windows/MainWindow.g.i.cs
+++ AsoulFollower/obj/Release/net6.0-windows/MainWindow.g.i.cs
@@ -205,7 +205,7 @@ namespace AsoulFollower {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AsoulFollower;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AsoulFollower;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
